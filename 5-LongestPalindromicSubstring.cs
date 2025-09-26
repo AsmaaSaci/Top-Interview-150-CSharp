a25abcb877f7ab288051900c54fd56c1af9bff20
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-26 09:45:13 +0100

    Create 5-LongestPalindromicSubstring.cs

---
+++ 5-LongestPalindromicSubstring.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+        if (string.IsNullOrEmpty(s)) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.Length; i++) {
+            int len1 = Expand(s, i, i);
+            int len2 = Expand(s, i, i + 1);
+            int len = Math.Max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.Substring(start, end - start + 1);
+    }
+
+    private int Expand(string s, int left, int right) {
+        while (left >= 0 && right < s.Length && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
