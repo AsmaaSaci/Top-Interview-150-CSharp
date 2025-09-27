c1bb7d612e7c40fd200230e45952496ee3eb3347
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-27 09:49:43 +0100

    Create 53-MaximumSubarray.cs

---
+++ 53-MaximumSubarray.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        for (int i = 1; i < nums.Length; i++) {
+            maxEndingHere = Math.Max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
