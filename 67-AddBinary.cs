088c90a8ba11a16a90200bff4bba7c3061df0612
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-27 11:19:30 +0100

    Create 67-AddBinary.cs

---
+++ 67-AddBinary.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string AddBinary(string a, string b) {
+        int i = a.Length - 1, j = b.Length - 1, carry = 0;
+        var sb = new System.Text.StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            sb.Insert(0, (char)('0' + (sum % 2)));
+            carry = sum / 2;
+        }
+
+        return sb.ToString();
+    }
+}
