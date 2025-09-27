4662dd94d6b5d939d0c2bb6226093466d70380ad
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-27 14:49:21 +0100

    Create 6-ZigzagConversion.cs

---
+++ 6-ZigzagConversion.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        var rows = new StringBuilder[Math.Min(numRows, s.Length)];
+        for (int i = 0; i < rows.Length; i++) rows[i] = new StringBuilder();
+        int curRow = 0;
+        bool goingDown = false;
+        foreach (char c in s) {
+            rows[curRow].Append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+        var result = new StringBuilder();
+        foreach (var row in rows) result.Append(row);
+        return result.ToString();
+    }
+}
