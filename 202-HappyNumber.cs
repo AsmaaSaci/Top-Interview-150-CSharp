5da10341b48ce69e0182cd328cf53ddf10f20131
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-28 12:02:33 +0100

    Create 202-HappyNumber.cs

---
+++ 202-HappyNumber.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsHappy(int n) {
+        var seen = new HashSet<int>();
+        while (n != 1 && !seen.Contains(n)) {
+            seen.Add(n);
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
