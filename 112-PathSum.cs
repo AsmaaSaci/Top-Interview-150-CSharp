ff0af2f2b4848c8fea606b7d0d6ddf11bce24b85
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-28 12:10:07 +0100

    Create 112-PathSum.cs

---
+++ 112-PathSum.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val == targetSum;
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
