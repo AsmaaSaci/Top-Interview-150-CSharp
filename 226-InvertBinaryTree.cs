64b563c49b7558678ec5022676640300dabc4dd6
Asmaa Saci <imyasmaa3@gmail.com>
2025-09-28 09:08:28 +0100

    Create 226-InvertBinaryTree.cs

---
+++ 226-InvertBinaryTree.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null) return null;
+        var temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
